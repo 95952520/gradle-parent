c4245cb954a8eda73cad80570db8a4f10d08a5b6
edwin.wang <edwin.wang@movit-tech.com>
2019-09-12 17:57:52 +0800

    令牌桶限流demo

---
+++ gradle-api/src/test/java/com/xuchen/gradle/api/limit/GuavaTest.java
@@ -0,0 +1,39 @@
+package com.xuchen.gradle.api.limit;
+
+import com.google.common.util.concurrent.RateLimiter;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * 基于令牌桶算法单机限流的demo
+ */
+@Slf4j
+public class GuavaTest {
+    public static void main(String[] args) {
+        //线程池
+        ExecutorService exec = Executors.newFixedThreadPool(10);
+        //速率是每秒只有3个许可
+        final RateLimiter rateLimiter = RateLimiter.create(3);
+
+        for (int i = 0; i < 100; i++) {
+            final int no = i;
+            Runnable runnable = () -> {
+                try {
+                    //获取许可
+                    rateLimiter.acquire();
+                    log.info("Accessing: " + no);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            };
+            //执行线程
+            exec.execute(runnable);
+        }
+        //退出线程池
+        exec.shutdown();
+    }
+}
