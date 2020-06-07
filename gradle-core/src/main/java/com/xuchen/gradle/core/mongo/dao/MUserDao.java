b8fe1d8976f3b247e2e25cbc56e378ca396adc30
wangxuchen <599268367@qq.com>
2020-06-07 10:53:31 +0800

    完善依赖

--- gradle-core/src/main/java/com/xuchen/gradle/core/mongo/dao/MUserDao.java
+++ gradle-core/src/main/java/com/xuchen/gradle/core/mongo/dao/MUserDao.java
@@ -1,17 +1,17 @@
-package com.xuchen.gradle.core.mongo.dao;
-
-import com.xuchen.gradle.core.mongo.entity.MUser;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import java.util.List;
-
-public interface MUserDao extends MongoRepository<MUser,Integer> {
-
-    List<MUser> findMUserBySex(Integer sex);
-
-    List<MUser> findMUserBySex(Integer sex, Pageable pageable);
-
-    List<MUser> findByAgeBetween(Integer begin,Integer end);
-
-}
+//package com.xuchen.gradle.core.mongo.dao;
+//
+//import com.xuchen.gradle.core.mongo.entity.MUser;
+//import org.springframework.data.domain.Pageable;
+//import org.springframework.data.mongodb.repository.MongoRepository;
+//
+//import java.util.List;
+//
+//public interface MUserDao extends MongoRepository<MUser,Integer> {
+//
+//    List<MUser> findMUserBySex(Integer sex);
+//
+//    List<MUser> findMUserBySex(Integer sex, Pageable pageable);
+//
+//    List<MUser> findByAgeBetween(Integer begin,Integer end);
+//
+//}
--- gradle-core/src/main/java/com/xuchen/gradle/core/mongo/entity/MUser.java
+++ gradle-core/src/main/java/com/xuchen/gradle/core/mongo/entity/MUser.java
@@ -1,19 +1,19 @@
-package com.xuchen.gradle.core.mongo.entity;
-
-import lombok.Data;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
-
-import java.time.LocalDateTime;
-
-@Document(collection = "user")
-@Data
-public class MUser {
-    @Id
-    private Integer id;
-    private String userName;
-    private Integer age;
-    private Integer sex;
-    private String desc;
-    private LocalDateTime createTime;
-}
+//package com.xuchen.gradle.core.mongo.entity;
+//
+//import lombok.Data;
+//import org.springframework.data.annotation.Id;
+//import org.springframework.data.mongodb.core.mapping.Document;
+//
+//import java.time.LocalDateTime;
+//
+//@Document(collection = "user")
+//@Data
+//public class MUser {
+//    @Id
+//    private Integer id;
+//    private String userName;
+//    private Integer age;
+//    private Integer sex;
+//    private String desc;
+//    private LocalDateTime createTime;
+//}
